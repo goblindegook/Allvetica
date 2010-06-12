93daf911f565bb40c93ef3f31ac21a517d416e92
goblindegook <goblindegook@gmail.com>
2010-06-12 20:56:54 +0100

    Rewrite the DOM CSS instead, dropped jQuery requirement

--- allvetica.js
+++ allvetica.js
@@ -1,9 +1,24 @@
-$('body').find('*').each(
-    function (i) {
-        if ($(this).css('font-family')) {
-            var fontFamily = $(this).css('font-family');
-            fontFamily = fontFamily.replace(/Arial/i, 'Helvetica');
-            $(this).css('font-family', fontFamily);
+if (document.styleSheets) {
+    var toReplace = /Arial/i;
+    var replacement = 'Helvetica';
+
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        var styleSheet = document.styleSheets[i];
+        
+        for (var j = 0; j < styleSheet.cssRules.length; j++) {
+            var cssRule = styleSheet.cssRules[j];
+            
+            // Replace in font-family attribute
+            if (cssRule.style.fontFamily.match( toReplace )) {
+                cssRule.style.fontFamily
+                    = cssRule.style.fontFamily.replace( toReplace, replacement );
+            }
+
+            // Replace in font attribute
+            if (cssRule.style.font.match( toReplace )) {
+                cssRule.style.font
+                    = cssRule.style.font.replace( toReplace, replacement );
+            }
         }
     }
-);
+}
